4184bc5ea060cfa974964d98098c564e2d740f65
David Cantrell <david@cantrell.org.uk>
2011-10-28 21:46:47 +0100

    Have Makefile.PL auto-build stuff as necessary; split Number::Phone::Country's data out

--- build-data.sh
+++ build-data.sh
@@ -2,13 +2,14 @@
 
 # THIS SHELL SCRIPT IS NOT INTENDED FOR END USERS OR FOR PEOPLE INSTALLING
 # THE MODULES, BUT FOR THE AUTHOR'S USE WHEN UPDATING THE DATA FROM OFCOM'S
-# PUBLISHED DATA.
+# AND libphonenumber's PUBLISHED DATA.
 
+# first get OFCOM data and update .../UK/Data.pm
 wget http://www.ofcom.org.uk/static/numbering/codelist.zip
-# wget http://www.ofcom.org.uk/telecoms/ioi/numbers/numbers_administered/codelist.zip
 unzip codelist.zip
 perl build-data.uk
 cat Data.pm temp.db > lib/Number/Phone/UK/Data.pm
-
 rm codelist.zip s[0123456789]*.txt sabc.txt Data.pm temp.db
 
+# now get an up-to-date libphonenumber
+(cd libphonenumber && svn up) || svn co http://libphonenumber.googlecode.com/svn/trunk libphonenumber
