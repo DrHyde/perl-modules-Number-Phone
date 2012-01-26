9cefc99e0e99bc068ad401ecbcff5e37e952a600
David Cantrell <david@cantrell.org.uk>
2012-01-26 10:33:41 +0000

    auto-rebuild in 'make dist'

--- build-data.sh
+++ build-data.sh
@@ -4,10 +4,12 @@
 # THE MODULES, BUT FOR THE AUTHOR'S USE WHEN UPDATING THE DATA FROM OFCOM'S
 # AND libphonenumber's PUBLISHED DATA.
 
+EXIT_STATUS=0
 # first get OFCOM data
 wget -q http://www.ofcom.org.uk/static/numbering/codelist.zip
 # if UK/Data.pm doesn't exist, or OFCOM's stuff is newer ...
 if test ! -e lib/Number/Phone/UK/Data.pm -o codelist.zip -nt lib/Number/Phone/UK/Data.pm; then
+  EXITSTATUS=1
   echo rebuilding lib/Number/Phone/UK/Data.pm
   unzip -q codelist.zip
   perl build-data.uk
@@ -23,6 +25,7 @@ rm codelist.zip
 
 # lib/Number/Phone/NANP/Data.pm doesn't exist, or if libphonenumber/resources/geocoding/en/1.txt is newer ...
 if test ! -e lib/Number/Phone/NANP/Data.pm -o libphonenumber/resources/geocoding/en/1.txt -nt lib/Number/Phone/NANP/Data.pm; then
+  EXITSTATUS=1
   echo rebuilding lib/Number/Phone/NANP/Data.pm
   perl build-data.nanp
 else
@@ -31,8 +34,11 @@ fi
 
 # lib/Number/Phone/StubCountry/MOCK.pm doesn't exist, or if libphonenumber/resources/PhoneNumberMetaData.xml is newer ...
 if test ! -e lib/Number/Phone/StubCountry/MOCK.pm -o libphonenumber/resources/PhoneNumberMetaData.xml -nt lib/Number/Phone/StubCountry/MOCK.pm; then
+  EXITSTATUS=1
   echo rebuilding lib/Number/Phone/StubCountry/\*.pm
   perl build-data.stubs
 else
   echo lib/Number/Phone/StubCountry/\*.pm are up-to-date
 fi
+
+exit $EXITSTATUS
