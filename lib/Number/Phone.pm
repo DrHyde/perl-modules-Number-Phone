62d3601ffdb4ea6e02d81c9b473e7b366647c121
David Cantrell <david@cantrell.org.uk>
2012-11-21 16:14:45 +0000

    remove embedded copy of DBM::Deep 0.9something, use latest CPAN version

--- lib/Number/Phone.pm
+++ lib/Number/Phone.pm
@@ -7,7 +7,7 @@ use Scalar::Util 'blessed';
 use Number::Phone::Country qw(noexport uk);
 use Number::Phone::StubCountry;
 
-our $VERSION = '2.1';
+our $VERSION = '2.2_TRIAL';
 
 my $NOSTUBS = 0;
 sub import {
