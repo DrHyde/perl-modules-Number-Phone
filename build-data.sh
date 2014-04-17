2b71ed16a77a7f0e478b078c6c9c0613f9a75f90
David Cantrell <david@cantrell.org.uk>
2014-04-17 21:58:18 +0100

    add --force option to build-data.sh; check both geocoding and metadata files when deciding whether NANP is up to date; metadata file got renamed upstream (only matters for devs on case-sensitive filesystems)

--- build-data.sh
+++ build-data.sh
@@ -4,6 +4,12 @@
 # THE MODULES, BUT FOR THE AUTHOR'S USE WHEN UPDATING THE DATA FROM OFCOM'S
 # AND libphonenumber's PUBLISHED DATA.
 
+if [ "$1" == "--force" ]; then
+  rm lib/Number/Phone/UK/Data.pm
+  rm lib/Number/Phone/NANP/Data.pm
+  rm lib/Number/Phone/StubCountry/KZ.pm
+fi
+
 EXIT_STATUS=0
 # first get OFCOM data
 curl -R -O -s http://www.ofcom.org.uk/static/numbering/codelist.zip
@@ -23,8 +29,8 @@ rm codelist.zip
 # now get an up-to-date libphonenumber
 (cd libphonenumber && svn -q up) || svn co http://libphonenumber.googlecode.com/svn/trunk libphonenumber
 
-# lib/Number/Phone/NANP/Data.pm doesn't exist, or if libphonenumber/resources/geocoding/en/1.txt is newer ...
-if test ! -e lib/Number/Phone/NANP/Data.pm -o libphonenumber/resources/geocoding/en/1.txt -nt lib/Number/Phone/NANP/Data.pm; then
+# lib/Number/Phone/NANP/Data.pm doesn't exist, or if libphonenumber/resources/geocoding/en/1.txt or PhoneNumberMetadata.xml is newer ...
+if test ! -e lib/Number/Phone/NANP/Data.pm -o libphonenumber/resources/geocoding/en/1.txt -nt lib/Number/Phone/NANP/Data.pm -o libphonenumber/resources/PhoneNumberMetadata.xml -nt lib/Number/Phone/NANP/Data.pm; then
   EXITSTATUS=1
   echo rebuilding lib/Number/Phone/NANP/Data.pm
   perl build-data.nanp
@@ -32,8 +38,8 @@ else
   echo lib/Number/Phone/NANP/Data.pm is up-to-date
 fi
 
-# lib/Number/Phone/StubCountry/KZ.pm doesn't exist, or if libphonenumber/resources/PhoneNumberMetaData.xml is newer ...
-if test ! -e lib/Number/Phone/StubCountry/KZ.pm -o libphonenumber/resources/PhoneNumberMetaData.xml -nt lib/Number/Phone/StubCountry/KZ.pm; then
+# lib/Number/Phone/StubCountry/KZ.pm doesn't exist, or if libphonenumber/resources/PhoneNumberMetadata.xml is newer ...
+if test ! -e lib/Number/Phone/StubCountry/KZ.pm -o libphonenumber/resources/PhoneNumberMetadata.xml -nt lib/Number/Phone/StubCountry/KZ.pm; then
   EXITSTATUS=1
   echo rebuilding lib/Number/Phone/StubCountry/\*.pm
   perl build-data.stubs
