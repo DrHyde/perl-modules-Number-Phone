430b2d7eaa9d9d91c178c43050bd7e98414fc0b4
David Cantrell <david@cantrell.org.uk>
2014-12-03 15:58:42 +0000

    bump version; update CHANGELOG for next release

--- lib/Number/Phone.pm
+++ lib/Number/Phone.pm
@@ -7,7 +7,7 @@ use Scalar::Util 'blessed';
 use Number::Phone::Country qw(noexport uk);
 use Number::Phone::StubCountry;
 
-our $VERSION = '3.0005';
+our $VERSION = '3.0006';
 
 my $NOSTUBS = 0;
 sub import {
