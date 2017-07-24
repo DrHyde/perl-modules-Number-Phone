be83fb2ab275ae43c2dccf368dd0663f3d0a2caa
David Cantrell <david@cantrell.org.uk>
2017-07-24 22:41:31 +0100

    can't use lib::abs, it's not portable to Windows

--- lib/Number/Phone/UK/Data.pm
+++ lib/Number/Phone/UK/Data.pm
@@ -7,12 +7,18 @@ our $VERSION = "2.0";
 
 use DBM::Deep;
 use File::ShareDir;
-use lib::abs;
+
+# giant ball of hate because lib::abs doesn't work on Windows
+use File::Spec::Functions qw(splitpath catpath catfile);
+use Cwd qw(abs_path);
+
+my $this_file = abs_path((caller(1))[1]);
+my $this_dir  = catpath((splitpath($this_file))[0,1]);
 
 my @candidate_files = (
-    lib::abs::path('.').'/../../../../lib/../share/Number-Phone-UK-Data.db',            # if this is $devdir/lib/...
-    lib::abs::path('.').'/../../../../../blib/lib/../../share/Number-Phone-UK-Data.db', # if this is $devdir/blib/lib/...
-    File::ShareDir::dist_dir('Number-Phone').'/Number-Phone-UK-Data.db',                # if this has been installed
+     catfile($this_dir, qw(.. .. .. .. lib .. share Number-Phone-UK-Data.db)),            # if this is $devdir/lib ...
+     catfile($this_dir, qw(.. .. .. .. .. blib lib .. .. share Number-Phone-UK-Data.db)), # if this is $devdir/blib/lib ...
+     catfile(File::ShareDir::dist_dir('Number-Phone'), 'Number-Phone-UK-Data.db'),        # if this has been installed
 );
 
 my $file = (grep { -e $_ } @candidate_files)[0];
@@ -23,5 +29,6 @@ if(!$file) {
     );
 }
 
+warn("Using $file\n");
 our $db = DBM::Deep->new($file);
 1;
