fdabfd2d5530278f06b570c5c9c934478e10fe8f
David Cantrell <david@cantrell.org.uk>
2018-01-08 22:21:41 +0000

    bump version number

--- lib/Number/Phone.pm
+++ lib/Number/Phone.pm
@@ -8,7 +8,7 @@ use Number::Phone::Country qw(noexport);
 use Number::Phone::StubCountry;
 
 # MUST be in format N.NNNN, see https://github.com/DrHyde/perl-modules-Number-Phone/issues/58
-our $VERSION = '3.4002';
+our $VERSION = '3.4003';
 
 my $NOSTUBS = 0;
 sub import {
