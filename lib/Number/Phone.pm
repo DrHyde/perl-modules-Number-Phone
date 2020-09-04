460730ac240e918b86632977c59aeabdb49e3949
David Cantrell <david@cantrell.org.uk>
2020-09-04 17:03:37 +0100

    book-keeping for release

--- lib/Number/Phone.pm
+++ lib/Number/Phone.pm
@@ -13,7 +13,7 @@ use Number::Phone::Country qw(noexport);
 use Number::Phone::StubCountry;
 
 # MUST be in format N.NNNN, see https://github.com/DrHyde/perl-modules-Number-Phone/issues/58
-our $VERSION = '3.6005';
+our $VERSION = '3.6006';
 
 my $NOSTUBS = 0;
 sub import {
