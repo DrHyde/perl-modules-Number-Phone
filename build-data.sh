b16e213c0d208a93aec4b4cf160439a985023203
David Cantrell <david@cantrell.org.uk>
2024-12-23 10:22:29 +0000

    Revert "Work-around for OFCOM's Cloudflare barrier"
    This reverts commit bc3e01e770088f851ddcb7a8bc138a9ebe4347eb.

--- build-data.sh
+++ build-data.sh
@@ -113,10 +113,9 @@ echo $LIBPHONENUMBERTAG > .libphonenumber-tag
 (
     cd data-files
 
-    # # OFCOM stuff commented out because of their shitty Cloudflare barrier
-    # wget -l 1 -nd --accept-regex telephone-numbers/.*.csv -r https://www.ofcom.org.uk/phones-and-broadband/phone-numbers/numbering-data
-    # for i in s[135789]*; do mv "$i" $(echo "$i"|sed 's/?.*//'); done
-    # rm s10-type*.csv numbering-data robots.txt
+    wget -l 1 -nd --accept-regex telephone-numbers/.*.csv -r https://www.ofcom.org.uk/phones-and-broadband/phone-numbers/numbering-data
+    for i in s[135789]*; do mv "$i" $(echo "$i"|sed 's/?.*//'); done
+    rm s10-type*.csv numbering-data robots.txt
 
     rm ThousandsBlockAssignment_All_Augmented.zip COCodeStatus_ALL.zip COCodeStatus_ALL.csv ThousandsBlockAssignment_All_Augmented.txt
     wget https://reports.nanpa.com/public/ThousandsBlockAssignment_All_Augmented.zip
@@ -126,7 +125,7 @@ echo $LIBPHONENUMBERTAG > .libphonenumber-tag
 )
 
 # stash the Unix epoch of the OFCOM data
-OFCOMDATETIME=$(cd data-files;perl -e 'print +(stat(shift))[9]' $(ls -rt S?.csv|tail -1))
+OFCOMDATETIME=$(cd data-files;perl -e 'print +(stat(shift))[9]' $(ls -rt s?.csv|tail -1))
 CADATETIME=$(cd data-files;perl -e 'print +(stat(shift))[9]' COCodeStatus_ALL.csv)
 USDATETIME=$(cd data-files;perl -e 'print +(stat(shift))[9]' ThousandsBlockAssignment_All_Augmented.txt)
 
@@ -138,10 +137,7 @@ if [ $(( $CURRENTDATETIME - $OFCOMDATETIME )) -gt $ONEMONTH -o \
      $(( $CURRENTDATETIME - $USDATETIME    )) -gt $ONEMONTH    \
    ]; then
     echo Data files are ANCIENT, check that the URLs are correct
-    echo For OFCOM data download zip file manually from
-    echo https://www.ofcom.org.uk/phones-and-broadband/phone-numbers/numbering-data/echo
-    echo and copy S?.csv to data-files/
-    ls -l data-files/S?.csv data-files/COCodeStatus_ALL.csv data-files/ThousandsBlockAssignment_All_Augmented.txt
+    ls -l data-files/s?.csv data-files/COCodeStatus_ALL.csv data-files/ThousandsBlockAssignment_All_Augmented.txt
     exit 1
 fi
 
@@ -150,12 +146,12 @@ fi
 if test ! -e share/Number-Phone-UK-Data.db -o \
   buildtools/Number/Phone/BuildHelpers.pm      -nt share/Number-Phone-UK-Data.db -o \
   libphonenumber/resources/geocoding/en/44.txt -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S1.csv -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S3.csv -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S5.csv -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S7.csv -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S8.csv -nt share/Number-Phone-UK-Data.db -o \
-  data-files/S9.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s1.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s3.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s5.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s7.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s8.csv -nt share/Number-Phone-UK-Data.db -o \
+  data-files/s9.csv -nt share/Number-Phone-UK-Data.db -o \
   build-data.uk     -nt share/Number-Phone-UK-Data.db;
 then
   if [ "$CI" != "True" ] && [ "$CI" != "true" ] && [ "$GITHUB_ACTIONS" != "true" ]; then
@@ -165,7 +161,7 @@ then
   if test ! -e share/Number-Phone-UK-Data.db; then
       echo "  because it doesn't exist"
   else
-      ls -ltr share/Number-Phone-UK-Data.db buildtools/Number/Phone/BuildHelpers.pm libphonenumber/resources/geocoding/en/44.txt data-files/S?.csv build-data.uk | \
+      ls -ltr share/Number-Phone-UK-Data.db buildtools/Number/Phone/BuildHelpers.pm libphonenumber/resources/geocoding/en/44.txt data-files/s?.csv build-data.uk | \
           sed 's/^/  /'
   fi
 
